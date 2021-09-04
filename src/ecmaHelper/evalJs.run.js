b43f2b97341bc5c8c6123d0c69fa447a17237642
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-09-04 04:02:02 -0700

    Dark-Fussion

--- src/ecmaHelper/evalJs.run.js
+++ src/ecmaHelper/evalJs.run.js
@@ -1,7 +1,7 @@
-// Ultroid - UserBot
-// Copyright (C) 2020 TeamUltroid
+// Dark Fussion - UserBot
+// Copyright (C) 2020 TeamFussion
 //
-// This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+// This file is a part of < https://github.com/TeamFussion/Darkfussion/ >
 // PLease read the GNU Affero General Public License in
 // <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
