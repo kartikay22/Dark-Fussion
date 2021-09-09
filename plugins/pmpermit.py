5001dd485bc1b63ae8caa1a474da6bd16ed0dc21
/\k$h@t <73880884+Akshat7678@users.noreply.github.com>
2021-09-09 10:09:06 +0530

    Update pmpermit.py

--- plugins/pmpermit.py
+++ plugins/pmpermit.py
@@ -783,7 +783,7 @@ async def in_pm_ans(event):
                         Button.inline("Warns", data=f"admin_only{from_user}"),
                         Button.inline(wrns, data="do_nothing"),
                     ],
-                    [Button.inline("Message 📫", data=f"m_{from_user}")],
+                    [Button.inline("Send this message to your Maser 👨‍💻", data=f"m_{from_user}")],
                 ],
             )
         ]
@@ -839,7 +839,7 @@ async def edt(event):
                 Button.inline("Warns", data=f"admin_only{from_user}"),
                 Button.inline(wrns, data="do_nothing"),
             ],
-            [Button.inline("Message 📫", data=f"m_{from_user}")],
+            [Button.inline("Send this message to your Master 👨‍💻", data=f"m_{from_user}")],
         ],
     )
 
